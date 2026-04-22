d407375cba2e989bab7cdeeead09a5e207f0b1cb
OC11444 <james24.gitau@student.cuk.ac.ke>
2026-04-22 16:48:28 +0300

    Fix MySQL SSL connection error

--- collab_task_manager/settings.py
+++ collab_task_manager/settings.py
@@ -97,7 +97,7 @@ if os.getenv("DATABASE_URL"):
     DATABASES = {
         "default": dj_database_url.config(
             conn_max_age=600,
-            ssl_require=True,
+            ssl_require=False,
         )
     }
 else:
